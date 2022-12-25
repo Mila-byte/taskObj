a81147178fceccca371305f1f82ae0a0decfe3fc
arch <kisharchi@gmail.com>
2022-12-25 12:18:02 +0100

    mod

--- index.js
+++ index.js
@@ -55,82 +55,83 @@
 
 // task 4
 
-// class Box {
-//     #height;
-//     #width;
-//     #color;
+class Box {
+    #height;
+    #width;
+    #color;
 
-//     constructor(width, height, color) {
-//         this.#width = width;
-//         this.#height = height;
-//         this.#color = color;
-//         this.drowingFigure(width, height, color);
-//     }
+    constructor(width, height, color) {
+        this.#width = width;
+        this.#height = height;
+        this.#color = color;
+        this.drowingFigure(width, height, color);
+    }
 
-//     drowingFigure(width, height, color) {
+    drowingFigure(width, height, color) {
         
-//         let box = document.querySelector('.box');
-//         box.addEventListener('click', function () {
-//             let box2 = document.querySelector('.box2')
-//             box2.style.cssText = `width: ${width}px; height: ${height}px; background-color: ${color}; margin: 60px auto`
-//         })
-//     }
-
-
-// }
-
-// class Circle extends Box {
-    
-//     constructor(width, height, color,borderRadius) {
-//         super(width, height, color);
-//         this.borderRadius = borderRadius;
-//         this.drowingFigure(width, height, color,borderRadius)
-//     }
-    
-//     drowingFigure(width, height, color, borderRadius) {
-//         super.drowingFigure(width, height, color)
-//         let box = document.querySelector('.box');
-//         box.addEventListener('click', () => {
-//             let box2 = document.querySelector('.box2')
-//             box2.style.cssText += `border-radius: ${borderRadius};`
-//         })
-//     }
-
-// }
-
-// const circle = new Circle(400, 400, "red", "50%")
-// const box = new Box(50, 50, "blue")
-
-// task 6
-
-let el = document.body;
-function create () {
-    const arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-
-    for(let i = 0; i < arr.length; i++) {
-        el = el.appendChild(document.createElement('div'));
-        el.id = arr[i];
-        el.innerText = arr[i];
-        el.style = `
-            margin-top: 70px;
-            width: 70px;
-            height: 70px;
-            background: black;
-            border-radius: 100%;    
-        `
-        el.onclick = function(event) {
-            console.log(this.id, event.target);
-            if(event.target.id === '5') {
-                event.stopPropagation()
-            }
-
-        }
+        let box = document.querySelector('.box');
+        box.addEventListener('click', function () {
+            let box2 = document.querySelector('.box2')
+            box2.style.cssText = `width: ${width}px; height: ${height}px; background-color: ${color}; margin: 60px auto`
+        })
     }
 
 
 }
 
-create()
+class Circle extends Box {
+    
+    constructor(width, height, color,borderRadius) {
+        super(width, height, color);
+        this.borderRadius = borderRadius;
+        this.drowingFigure(width, height, color,borderRadius)
+    }
+    
+    drowingFigure(width, height, color, borderRadius) {
+        super.drowingFigure(width, height, color)
+        let box = document.querySelector('.box');
+        box.addEventListener('click', () => {
+            let box2 = document.querySelector('.box2')
+            box2.style.cssText += `border-radius: ${borderRadius};`
+        })
+    }
+
+}
+// drowing circle
+const circle = new Circle(400, 400, "red", "50%")
+// drowing square
+const box = new Box(50, 50, "blue")
+
+// task 6
+
+// let el = document.body;
+// function create () {
+//     const arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+//     for(let i = 0; i < arr.length; i++) {
+//         el = el.appendChild(document.createElement('div'));
+//         el.id = arr[i];
+//         el.innerText = arr[i];
+//         el.style = `
+//             margin-top: 70px;
+//             width: 70px;
+//             height: 70px;
+//             background: black;
+//             border-radius: 100%;    
+//         `
+//         el.onclick = function(event) {
+//             console.log(this.id, event.target);
+//             if(event.target.id === '5') {
+//                 event.stopPropagation()
+//             }
+
+//         }
+//     }
+
+
+// }
+
+// create()
 
 
 
